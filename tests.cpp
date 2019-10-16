309a55fddde646b8ed66af2038d5742a5ba9aafb
Georg <georgpohl@gmx.de>
2019-10-16 15:14:49 +0000

    new

--- tests.cpp
+++ tests.cpp
@@ -3,6 +3,8 @@
  * Filename: tests.cpp
  */
  #include <iostream>
+ #include <queue>
+ #include <stack>
  #include <map>
  using namespace std;
 
@@ -12,10 +14,24 @@
 /* Instance Variables */
 
 /* Prototypes */
+void collapse(queue<int> &);
 
 
 int main() {
+    queue<int> qu;
 
+    qu.push(7);
+    qu.push(2);
+    qu.push(8);
+    qu.push(9);
+    qu.push(4);
+    qu.push(13);
+    qu.push(7);
+    qu.push(1);
+    qu.push(9);
+    qu.push(10);
+
+    collapse(qu);
 
 
     cout << endl;
@@ -23,3 +39,15 @@ int main() {
 } // main
 
 /* Functions */
+void collapse(queue<int> &q) {
+    int temp;
+    stack<int> st1;
+    stack<int> str2;
+
+    while (!q.empty()) {
+        temp = q.pop();
+        st1.push(temp);
+    }
+    cout << st1.top() << endl;
+}
+// {7, 2, 8, 9, 4, 13, 7, 1, 9, 10}
