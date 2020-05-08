112a15316c7940b4d9b7001ea03bf67a2afa150b
Georg <georgpohl@gmx.de>
2020-05-08 04:52:21 +0000

    Created new main template

--- tests.cpp
+++ tests.cpp
@@ -1,51 +1,26 @@
 /*
- * Copyright: Georg Pohl, 70174 Stuttgart, et al.
- * Filename: CheckoutLineClass.cpp
- */
- #include <iostream>
- #include <iomanip>
- #include "queue.h"
- #include "random.h"
- using namespace std;
-
- /* CONSTANTS */
- const double ARRIVAL_PROBABILITY = 0.05;
- const int MIN_SERVICE_TIME = 5;
- const int MAX_SERVICE_TIME = 15;
- const int SIMULATION_TIME = 2000;
-
-/* Instance Variables */
-
-/* Prototypes */
-
-/*
- * Class: CheckoutLineSimulation
- * -----------------------------
- * This class encapsulates the code and data for the simulation.
+ * Copyright May 2020: Georg Pohl, 70174 Stuttgart
+ * File: chapter05_exercise15.cpp
+ * ------------------------------
+ * Rolling stack
  */
 
- class CheckoutLineSimulation {
- public:
-    void runSimulation(int &nServed, int &totalWait, int &totalLength) {
-        Queue<int> queue;
-    }
-    void printReport() {
+#include <iostream>
 
-    }
 
- private:
-    int nServed;        /* Number of customers served */
-    int totalWait;      /* Sum of all customer waiting times */
-    int totalLength;    /* Sum of the queue length at each time step */
- }
+using namespace std;
 
- /* Main Program */
+/* CONSTANTS */
 
- int main() {
-     CheckoutLineSimulation simulation;
-     simulation.runSimulation();
-     simulation.printReport();
-     return 0;
- }
+/* STRUCTS */
 
-/* Functions */
+/* PROTOTYPES */
+
+int main(int argc, char **argv) { 
+    
+  
+  return EXIT_SUCCESS;
+} // main {} [] \n |
+
+/* FUNCTIONS */
+
